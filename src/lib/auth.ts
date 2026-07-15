b59bd6274e9aa57ebdeff892cc1911906b2c3464
mehedyPUST <mehedy.pust@gmail.com>
2026-07-16 04:26:35 +0600

    auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-    baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_APP_URL,
+    baseURL: process.env.BETTER_AUTH_URL,
     secret: process.env.BETTER_AUTH_SECRET,
     plugins: [nextCookies()],
     session: {
